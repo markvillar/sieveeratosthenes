77c1c5ab8a61971117ebd0680cdfe77fd152c062
Mark Villar <markcvillar@gmail.com>
2019-04-29 18:45:57 +0100

    renamed n to inputNumber

--- src/SieveEratosthenes.java
+++ src/SieveEratosthenes.java
@@ -11,28 +11,28 @@
 
 class SieveEratosthenes extends Thread {
 
-    void sieveOfEratosthenes(int n) {
+    void sieveOfEratosthenes(int inputNumber) {
         // Create a boolean array "prime[0..n]" and initialize 
         // all entries it as true. A value in prime[i] will 
         // finally be false if i is Not a prime, else true. 
-        boolean prime[] = new boolean[n + 1];
+        boolean prime[] = new boolean[inputNumber + 1];
         
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < inputNumber; i++) {
             prime[i] = true;
         }
 
-        for (int p = 2; p * p <= n; p++) {
+        for (int p = 2; p * p <= inputNumber; p++) {
             // If prime[p] is not changed, then it is a prime 
             if (prime[p] == true) {
                 // Update all multiples of p 
-                for (int i = p * 2; i <= n; i += p) {
+                for (int i = p * 2; i <= inputNumber; i += p) {
                     prime[i] = false;
                 }
             }
         }
 
         // Print all prime numbers 
-        for (int i = 2; i <= n; i++) {
+        for (int i = 2; i <= inputNumber; i++) {
             if (prime[i] == true) {
                 System.out.print(i + " ");
             }
