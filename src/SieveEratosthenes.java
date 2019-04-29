ce618141ec8c17f1b5054235f6c44f650da0fe36
Mark Villar <markcvillar@gmail.com>
2019-04-29 15:49:29 +0100

    Removed package

--- src/SieveEratosthenes.java
+++ src/SieveEratosthenes.java
@@ -1,36 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
 /**
  *
  * @author UP773229
  */
 import java.util.Scanner;
-//import java.lang.Math.sqrt;
 
 class SieveEratosthenes {
 
-    void sieveOfEratosthenes(int inputNumber) {
-        // Create a boolean array "prime[0..inputNumber]" and initialize
-        // all entries it as true. A value in prime[i] will
-        // finally be false if i is Not a prime, else true.
-        boolean prime[] = new boolean[inputNumber + 1];
+    void sieveOfEratosthenes(int n) {
+        // Create a boolean array "prime[0..n]" and initialize 
+        // all entries it as true. A value in prime[i] will 
+        // finally be false if i is Not a prime, else true. 
+        boolean prime[] = new boolean[n + 1];
         
-        //Set all to true from 0 up to (inputNumber-1)
-        for (int i = 0; i < inputNumber; i++) {
+        for (int i = 0; i < n; i++) {
             prime[i] = true;
         }
-        
-        //Test for primeness
-        for (int p = 2; p * p <= inputNumber; p++) {
-            // If prime[p] is not changed, then it is a prime
+
+        for (int p = 2; p * p <= n; p++) {
+            // If prime[p] is not changed, then it is a prime 
             if (prime[p] == true) {
-                // Update all multiples of p
-                for (int i = p * 2; i <= inputNumber; i += p) {
+                // Update all multiples of p 
+                for (int i = p * 2; i <= n; i += p) {
                     prime[i] = false;
                 }
             }
         }
 
         // Print all prime numbers 
-        for (int i = 2; i <= inputNumber; i++) {
+        for (int i = 2; i <= n; i++) {
             if (prime[i] == true) {
                 System.out.print(i + " ");
             }
@@ -42,11 +46,10 @@ class SieveEratosthenes {
         
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number: ");
-        //int n = sc.nextInt();
-        Integer n = 8;
+        int n = sc.nextInt();
         
         System.out.print("Following are the prime numbers ");
-        System.out.println("are less than " + n);
+        System.out.println("smaller than or equal to " + n);
         SieveEratosthenes g = new SieveEratosthenes();
         g.sieveOfEratosthenes(n);
     }
