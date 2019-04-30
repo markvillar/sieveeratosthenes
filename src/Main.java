6de61b0c3f8a9fdd8a2ebe3a75f24c9738087744
Mark Villar <markcvillar@gmail.com>
2019-04-30 21:50:42 +0100

    Prime Number Checker

--- src/Main.java
+++ src/Main.java
@@ -4,23 +4,78 @@
  * and open the template in the editor.
  */
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 /**
  *
  * @author UP773229
  */
 public class Main {
 
+    public static Boolean checkIfPrime(int inputNumber) {
+
+        if (inputNumber <= 3) {
+            return inputNumber > 1;
+        } else if (inputNumber % 2 == 0 || inputNumber % 3 == 0) {
+            return false;
+        }
+
+        int number = 5;
+
+        while (number * number <= inputNumber) {
+
+            if (inputNumber % number == 0 || inputNumber % (number + 2) == 0) {
+                return false;
+            }
+
+            number = number + 6;
+        }
+
+        return true;
+    }
+
     // Driver Program to test above function 
     public static void main(String args[]) {
 
-        int inputNumber = 140;
+        int cores = Runtime.getRuntime().availableProcessors();
+
+        int inputNumber = 10;
+
+        //Generate ArrayList of boolean values
+        List<Boolean> primes = new ArrayList<>(Arrays.asList(new Boolean[inputNumber + 1]));
+        //Set all boolean values to false
+        Collections.fill(primes, false);
+
+        //Generate numbers from 0 to inputNumber
+        List<Integer> generatedNumbers = IntStream.range(1, (inputNumber + 1)).boxed().collect(Collectors.toList());
+
+        //Temporary
+        int listLength = generatedNumbers.size();
+
+        //Test the values
+        generatedNumbers.forEach((number) -> {
+            boolean result = checkIfPrime(number);
+            primes.set(number, result);
+        });
+
+        //Output the results
+        for (int iterator = 0; iterator < primes.size(); iterator++) {
+
+            boolean result = primes.get(iterator);
+
+            if (result == true) {
+                System.out.println(iterator + " is Prime");
+            } else {
+                System.out.println(iterator + " is not Prime");
+            }
+
+        }
         
-        System.out.print("Following are the prime numbers ");
-        System.out.println("smaller than or equal to " + inputNumber);
-
-        SieveEratosthenes s = new SieveEratosthenes();
-
-        s.sieveOfEratosthenes(inputNumber);
     }
 
 }
